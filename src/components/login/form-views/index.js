8738b6236983336d89a9ce9a6c792c469a6eb6a7
Major Pinky <107479804+JosephWagio@users.noreply.github.com>
2022-12-29 13:50:32 +0100

    Delete src directory

--- src/components/login/form-views/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoginForm } from "./LoginForm";
-export { default as VerificationForm } from "./VerificationForm";
-export { default as ForgotPasswordForm } from "./ForgotPasswordForm";
-export { default as ResetPasswordForm } from "./ResetPasswordForm";
